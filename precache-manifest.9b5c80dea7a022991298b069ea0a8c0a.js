6c2fa3d0e8408370869889efa1506202ab820b8e
Jessica Iliff <jdiliff@Jessicas-MacBook-Pro-2.local>
2019-02-16 12:37:02 -0600

    Updates

---
+++ precache-manifest.9b5c80dea7a022991298b069ea0a8c0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "18a7584d46ca53953aab9bdacab75921",
+    "url": "/myportfolio/static/media/pradyse.18a7584d.otf"
+  },
+  {
+    "revision": "6ee8cf4776654a73615b458a402a2744",
+    "url": "/myportfolio/static/media/kd.6ee8cf47.png"
+  },
+  {
+    "revision": "01a2cde5509f2f4750d6",
+    "url": "/myportfolio/static/js/runtime~main.01a2cde5.js"
+  },
+  {
+    "revision": "c0f0570908192c73ae61",
+    "url": "/myportfolio/static/js/main.c0f05709.chunk.js"
+  },
+  {
+    "revision": "d0154aeca8b4383960db",
+    "url": "/myportfolio/static/js/2.d0154aec.chunk.js"
+  },
+  {
+    "revision": "c0f0570908192c73ae61",
+    "url": "/myportfolio/static/css/main.d53adf5a.chunk.css"
+  },
+  {
+    "revision": "cddb560975a978a26300201218d5cde4",
+    "url": "/myportfolio/index.html"
+  }
+];
